70936ad784aacc63a074daa9a54551ae399ea243
Jaromir Jakacki <jjakacki@iopan.gda.pl>
2022-10-21 08:48:48 +0200

    modules.sh

--- apps/modules.sh
+++ apps/modules.sh
@@ -6,7 +6,7 @@ ulimit -l unlimited
 ulimit -s unlimited
 export KMP_STACKSIZE=274877906944
 
-export HOME=/users/kdm/mmuzyka
+export HOME=/users/kdm/jakacki
 
 module purge
 module load tryton/netcdf/4.7.4-intel19-mpi-iopan
